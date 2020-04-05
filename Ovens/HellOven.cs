28de6077e6c7e411549b9f8f70571bb654c32c7a
Planterror <47764567+Plantterror@users.noreply.github.com>
2020-04-05 17:02:03 -0600

    Probably about the time when

--- Ovens/HellOven.cs
+++ Ovens/HellOven.cs
@@ -3,6 +3,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
+using static Terraria.ModLoader.ModContent;
 
 namespace WeaponOven.Ovens
 {
@@ -58,7 +59,7 @@ namespace WeaponOven.Ovens
 		}
 		public override void KillMultiTile(int i, int j, int frameX, int frameY)
 		{
-
+			Item.NewItem(i * 16, j * 16, 48, 32, ItemType<HellOvenItem>());
 		}
 	}
 }
